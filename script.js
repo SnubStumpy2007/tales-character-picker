75f3f685e07e02109c9b9d5b83907fabcba39134
SnubStumpy2007 <joshua192007@gmail.com>
2024-01-25 19:08:07 -0500

    update

---
+++ script.js
@@ -0,0 +1,25 @@
+let choice = prompt(
+  "Enter a character name from the video game Tales of Xillia"
+);
+
+switch (choice) {
+  case "Jude Mathis":
+    console.log("Jude Mathis");
+    break;
+  case "Leia Rolando":
+    console.log("Leia Rolando");
+    break;
+  case "Elize Lutus":
+    console.log("Elize Lutus");
+    break;
+  case "Alvin":
+    console.log("Alvin");
+    break;
+  case "Rowen Ilbert":
+    console.log("Rowen Ilbert");
+    break;
+  case "Milla Maxwell":
+    console.log("Milla Maxwell");
+  default:
+    console.log("invalid choice");
+}
