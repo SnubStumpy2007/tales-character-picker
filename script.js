e6d8ed4fa02334bf49ddf65b13e6f2b9a795727f
SnubStumpy2007 <joshua192007@gmail.com>
2024-01-25 19:39:48 -0500

    update

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-let choice = document.getElementsByClassName("choice")
-let btn = document.getElementsByClassName("btn")
 
-
-switch (choice) {
-  case "Jude Mathis":
-    console.log("Jude Mathis");
-    break;
-  case "Leia Rolando":
-    console.log("Leia Rolando");
-    break;
-  case "Elize Lutus":
-    console.log("Elize Lutus");
-    break;
-  case "Alvin":
-    console.log("Alvin");
-    break;
-  case "Rowen Ilbert":
-    console.log("Rowen Ilbert");
-    break;
-  case "Milla Maxwell":
-    console.log("Milla Maxwell");
-    break;
-  default:
-    console.log("invalid choice");
-}
+document.getElementById("btn").addEventListener("click", function() {
+    let choice = document.getElementsByClassName("choice")
+    switch (choice) {
+        case "Jude Mathis":
+          console.log("Jude Mathis");
+          break;
+        case "Leia Rolando":
+          console.log("Leia Rolando");
+          break;
+        case "Elize Lutus":
+          console.log("Elize Lutus");
+          break;
+        case "Alvin":
+          console.log("Alvin");
+          break;
+        case "Rowen Ilbert":
+          console.log("Rowen Ilbert");
+          break;
+        case "Milla Maxwell":
+          console.log("Milla Maxwell");
+          break;
+        default:
+          console.log("invalid choice");
+      }
+      
+  });
